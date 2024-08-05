310736dc93e5b263f41b48aab58234985b289b95
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-08-05 13:43:03 +0700

    Update client.py

--- pyrogram/client.py
+++ pyrogram/client.py
@@ -184,8 +184,8 @@ class Client(Methods):
             Defaults to 1.
     """
 
-    APP_VERSION = f"ArgusPython {__version__}"
-    DEVICE_MODEL = f"ArgusUbot"
+    APP_VERSION = f"ArabPython {__version__}"
+    DEVICE_MODEL = f"ArabUbot"
     SYSTEM_VERSION = f"Linux 64"
 
     LANG_CODE = "en"
